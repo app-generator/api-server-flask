973f2c96fb0f161f98bf742e33aaf1a6f79ade51
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-02 20:33:12 +0200

    Update config.py

--- api/config.py
+++ api/config.py
@@ -11,8 +11,10 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class BaseConfig():
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'apidata.db')
+    SQLALCHEMY_DATABASE_URI        = 'sqlite:///' + os.path.join(BASE_DIR, 'apidata.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "flask-app-secret-key-change-it"
-    JWT_SECRET_KEY = "jwt-app-secret-key-change-it"
+    
+    SECRET_KEY     = os.getenv('SECRET_KEY'    , 'S#perS3crEt_913' )
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'S#perS3crEt_JWT' )
+    
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
