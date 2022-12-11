3c7c1d7bef9a474287a81983cc248eaf8b558f57
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-11 12:23:59 +0200

    Update config.py

--- api/config.py
+++ api/config.py
@@ -14,9 +14,9 @@ class BaseConfig():
     SQLALCHEMY_DATABASE_URI        = 'sqlite:///' + os.path.join(BASE_DIR, 'apidata.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    SECRET_KEY     = os.getenv('SECRET_KEY'    , 'S#perS3crEt_913' )
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'S#perS3crEt_JWT' )
-    GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
-    GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
+    SECRET_KEY           = os.getenv('SECRET_KEY'    , 'S#perS3crEt_913' )
+    JWT_SECRET_KEY       = os.getenv('JWT_SECRET_KEY', 'S#perS3crEt_JWT' )
+    GITHUB_CLIENT_ID     = os.getenv('GITHUB_CLIENT_ID')
+    GITHUB_CLIENT_SECRET = os.getenv('GITHUB_SECRET_KEY')
     
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
